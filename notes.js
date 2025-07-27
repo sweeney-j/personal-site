4ccd014972a9a0ec7cea301c954e9ddec07addee
sweeney-j <jedsweeney5@gmail.com>
2025-07-27 16:40:25 -0400

    fix: dynamic blog injection fix (still need to fix async json fetch)

--- notes.js
+++ notes.js
@@ -1,13 +1,20 @@
-var notesJson = fetch('notes.json').then(res => res.json());
+// var notesJson = fetch('notes.json').then(res => res.json());
 
-console.log(notesJson); 
+var notesJson = '{"posts": [ { "title": "Example Title", "description": "This is example preview text to test my automated system", "date":"27.07.2025" }, { "title": "Example Title 2.0", "description": "This is example preview text to test my automated system", "date":"27.07.2025" } ] }'
 
-const postList = document.querySelector("#posts"); 
+var test = JSON.parse(notesJson); 
 
-notesJson.posts.forEach(post => {
-    postList.innerHTML += '<div class="blog-post"></div>';
+const postList = document.getElementById("posts"); 
+
+var uniqueId = 0; 
+test.posts.forEach(post => {
+    var blogClassName = `<div class="blog-${uniqueId}"></div>`;
+    postList.innerHTML += blogClassName;
+    var currentBlog = document.getElementsByClassName(`blog-${uniqueId}`); 
+    var blogTitle = `<h2 class="blog-heading">${post.title}</h2>`; 
+    var blogAbout = `<p class="blog-about">${post.description}</p>`;
+    currentBlog[0].innerHTML = blogTitle + blogAbout; 
+    uniqueId += 1; 
 }); 
-
-console.log(postList); 
 
 
