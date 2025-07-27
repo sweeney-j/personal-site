2caa5e24bcc8a256383431d1bb2a53afdf67c5c4
sweeney-j <jedsweeney5@gmail.com>
2025-07-27 19:42:32 -0400

    feat: dynamic blog elements from Json

--- notes.js
+++ notes.js
@@ -1,20 +1,15 @@
-// var notesJson = fetch('notes.json').then(res => res.json());
-
-var notesJson = '{"posts": [ { "title": "Example Title", "description": "This is example preview text to test my automated system", "date":"27.07.2025" }, { "title": "Example Title 2.0", "description": "This is example preview text to test my automated system", "date":"27.07.2025" } ] }'
-
-var test = JSON.parse(notesJson); 
-
-const postList = document.getElementById("posts"); 
-
-var uniqueId = 0; 
-test.posts.forEach(post => {
-    var blogClassName = `<div class="blog-${uniqueId}"></div>`;
-    postList.innerHTML += blogClassName;
-    var currentBlog = document.getElementsByClassName(`blog-${uniqueId}`); 
-    var blogTitle = `<h2 class="blog-heading">${post.title}</h2>`; 
-    var blogAbout = `<p class="blog-about">${post.description}</p>`;
-    currentBlog[0].innerHTML = blogTitle + blogAbout; 
-    uniqueId += 1; 
-}); 
-
-
+fetch('notes.json')
+    .then(res => res.json())
+    .then(data => { 
+        const postList = document.getElementById("posts"); 
+        var uniqueId = 0; 
+        data.posts.forEach(post => {
+            var blogClassName = `<div class="blog-${uniqueId}"></div>`;
+            postList.innerHTML += blogClassName;
+            var currentBlog = document.getElementsByClassName(`blog-${uniqueId}`); 
+            var blogTitle = `<h2 class="blog-heading">${post.title}</h2>`; 
+            var blogAbout = `<p class="blog-about">${post.description}</p>`;
+            currentBlog[0].innerHTML = blogTitle + blogAbout; 
+            uniqueId += 1; 
+        }); 
+    }); 
